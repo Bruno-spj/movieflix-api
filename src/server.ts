be61a5ec3e3e9ff6b02a9eb04e943f5e5a2da312
Bruno-spj <bruno_spj@hotmail.com>
2025-03-20 02:24:25 -0300

    Adicionada correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(express.json());
+
 app.get("/movies", async (req, res) => {
   const movies = await prisma.movie.findMany({
     orderBy: {
@@ -18,27 +20,36 @@ app.get("/movies", async (req, res) => {
   res.json(movies);
 });
 
+app.use(express.json());
 app.post("/movies", async (req, res) => {
-  const movies = await prisma.movie.create({
-    data: {
-      title: "A Paixão de Cristo",
-      genre_id: 4,
-      language_id: 2,
-      oscar_count: 3,
-      release_date: new Date(2004, 2, 19),
+  const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+  // Verifica se já existe um filme com o mesmo título
+  const existingMovie = await prisma.movie.findFirst({
+    where: {
+      title: { equals: title, mode: "insensitive" },
     },
   });
-  res.status(201).json(movies);
-});
 
-app.delete("/movies", async (req, res) =>{
-  const deleteMovie = await prisma.movie.delete({
-    where: {
-      id: 12,
+  if (existingMovie) {
+    return res
+      .status(409)
+      .json({ message: "Já existe um filme com esse título." });
+  }
+
+  // Criação do novo filme caso não exista duplicação
+  const movie = await prisma.movie.create({
+    data: {
+      title,
+      genre_id,
+      language_id,
+      oscar_count,
+      release_date: new Date(release_date),
     },
-  })
-  res.status(500).json()
-})
+  });
+
+  res.status(201).json(movie);
+});
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta http://localhost:${port}`);
