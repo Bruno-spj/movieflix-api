c04a79d4017a10bca12e9df1ab7bccb577f29747
Bruno-spj <bruno_spj@hotmail.com>
2025-03-25 02:36:51 -0300

    adicionada remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -51,6 +51,54 @@ app.post("/movies", async (req, res) => {
   res.status(201).json(movie);
 });
 
+app.put("/movies/:id", async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const movie = await prisma.movie.findUnique({
+      where: { id },
+    });
+
+    if (!movie) {
+      return res.status(404).send({ message: "Filme não encontrado" });
+    }
+
+    const data = { ...req.body };
+    data.release_date = data.release_date
+      ? new Date(data.release_date)
+      : undefined;
+
+    await prisma.movie.update({ where: { id }, data });
+
+    res.status(200).send({ message: "Filme atualizado com sucesso" });
+  } catch (error) {
+    res.status(500).send({ message: "Falha ao atualizar o registro" });
+  }
+});
+
+app.delete("/movies/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  try {
+    const movie = await prisma.movie.findUnique({
+      where: {
+        id,
+      },
+    });
+  
+    if (!movie) {
+      return res.status(404).send({ message: "Filme não encontrado" });
+    }
+    await prisma.movie.delete({
+      where: {
+        id,
+      },
+    });
+  } catch (error) {
+    return res.status(500).send({ message: 'Falha ao remover o filme' });
+  }
+  res.status(200).send({message: "Filme deletado com sucesso"});
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta http://localhost:${port}`);
 });
